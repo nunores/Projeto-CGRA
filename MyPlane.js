d3cceb30d82625a3692ace90a633084243536ab6
unknown <n.resende2000@gmail.com>
2020-05-17 10:39:37 +0100

    Dois lados no plane?

--- MyPlane.js
+++ MyPlane.js
@@ -1,62 +1,65 @@
 /** Represents a plane with nrDivs divisions along both axis, with center at (0,0) */
 class MyPlane extends CGFobject {
-    constructor(scene, nrDivs, minS, maxS, minT, maxT) {
-        super(scene);
-        // nrDivs = 1 if not provided
-        nrDivs = typeof nrDivs !== 'undefined' ? nrDivs : 1;
-        this.nrDivs = nrDivs;
-        this.patchLength = 1.0 / nrDivs;
-        this.minS = minS || 0;
-        this.maxS = maxS || 1;
-        this.minT = minT || 0;
-        this.maxT = maxT || 1;
-        this.q = (this.maxS - this.minS) / this.nrDivs;
-        this.w = (this.maxT - this.minT) / this.nrDivs;
-        this.initBuffers();
+  constructor(scene, nrDivs, minS, maxS, minT, maxT) {
+    super(scene);
+    // nrDivs = 1 if not provided
+    nrDivs = typeof nrDivs !== "undefined" ? nrDivs : 1;
+    this.nrDivs = nrDivs;
+    this.patchLength = 1.0 / nrDivs;
+    this.minS = minS || 0;
+    this.maxS = maxS || 1;
+    this.minT = minT || 0;
+    this.maxT = maxT || 1;
+    this.q = (this.maxS - this.minS) / this.nrDivs;
+    this.w = (this.maxT - this.minT) / this.nrDivs;
+    this.initBuffers();
+  }
+  initBuffers() {
+    // Generate vertices, normals, and texCoords
+    this.vertices = [];
+    this.normals = [];
+    this.texCoords = [];
+    var yCoord = 0.5;
+    for (var j = 0; j <= this.nrDivs; j++) {
+      var xCoord = -0.5;
+      for (var i = 0; i <= this.nrDivs; i++) {
+        this.vertices.push(xCoord, yCoord, 0);
+        this.normals.push(0, 0, 1);
+        this.texCoords.push(this.minS + i * this.q, this.minT + j * this.w);
+        xCoord += this.patchLength;
+      }
+      yCoord -= this.patchLength;
     }
-    initBuffers() {
-        // Generate vertices, normals, and texCoords
-        this.vertices = [];
-        this.normals = [];
-        this.texCoords = [];
-        var yCoord = 0.5;
-        for (var j = 0; j <= this.nrDivs; j++) {
-            var xCoord = -0.5;
-            for (var i = 0; i <= this.nrDivs; i++) {
-                this.vertices.push(xCoord, yCoord, 0);
-                this.normals.push(0, 0, 1);
-                this.texCoords.push(this.minS + i * this.q, this.minT + j * this.w);
-                xCoord += this.patchLength;
-            }
-            yCoord -= this.patchLength;
-        }
-        // Generating indices
-        this.indices = [];
+    // Generating indices
+    this.indices = [];
 
-        var ind = 0;
-        for (var j = 0; j < this.nrDivs; j++) {
-            for (var i = 0; i <= this.nrDivs; i++) {
-                this.indices.push(ind);
-                this.indices.push(ind + this.nrDivs + 1);
-                ind++;
-            }
-            if (j + 1 < this.nrDivs) {
-
-                this.indices.push(ind + this.nrDivs);
-                this.indices.push(ind);
-            }
-        }
-        this.primitiveType = this.scene.gl.TRIANGLE_STRIP;
-        this.initGLBuffers();
+    var ind = 0;
+    for (var j = 0; j < this.nrDivs; j++) {
+      for (var i = 0; i <= this.nrDivs; i++) {
+        this.indices.push(ind);
+        this.indices.push(ind + this.nrDivs + 1);
+        ind++;
+      }
+      if (j + 1 < this.nrDivs) {
+        this.indices.push(ind + this.nrDivs);
+        this.indices.push(ind);
+      }
     }
 
-    setFillMode() {
-        this.primitiveType = this.scene.gl.TRIANGLE_STRIP;
-    }
+    // TODO: Testar se isto põe a bandeira com dois lados
+    /*for (var i = this.indices.length; i >= 0; i--) {
+      this.indices.push(this.indices[i]);
+    }*/
 
-    setLineMode() {
-        this.primitiveType = this.scene.gl.LINES;
-    };
+    this.primitiveType = this.scene.gl.TRIANGLE_STRIP;
+    this.initGLBuffers();
+  }
 
+  setFillMode() {
+    this.primitiveType = this.scene.gl.TRIANGLE_STRIP;
+  }
+
+  setLineMode() {
+    this.primitiveType = this.scene.gl.LINES;
+  }
 }
-
